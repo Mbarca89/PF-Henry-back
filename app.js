8a098ee81a7e56f5ac30bdef5b9871e57aa7484d
Mbarca89 <mauriciobarca1989@gmail.com>
2023-06-12 20:08:20 +0200

    installed mongo

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import morgan from "morgan";
+import fileUpload from "express-fileupload";
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  fileUpload({
+    tempFileDir: "./upload",
+    useTempFiles: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Routes
+
+
+export { app };
