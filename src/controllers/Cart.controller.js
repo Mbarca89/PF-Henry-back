ba572ce226787e77376b8e7cd031e8a25010991b
Mbarca89 <mauriciobarca1989@gmail.com>
2023-06-15 10:21:37 +0200

    added getCart route and crontroller

--- src/controllers/Cart.controller.js
+++ src/controllers/Cart.controller.js
@@ -1,19 +1,31 @@
 import Cart from "../models/Cart.js";
 import Product from "../models/Product.js";
 
-const addProduct = async (req,res) => {
+const getCart = async (req, res) => {
+    try {
+        const { userId } = req.params
+        if (!userId) throw Error('Falta el ID de usuario')
+        const userCart = await Cart.findOne({ user: userId })
+        if (!userCart) throw Error('No existe el carrito')
+        return res.status(200).json(userCart)
+    } catch (error) {
+        return res.status(400).send(error.message)
+    }
+}
+
+const addProduct = async (req, res) => {
     try {
         const { id, quantity, userId } = req.body
-        
+
         const product = await Product.findById(id)
         if (!product) throw Error('Producto no encontrado')
 
-        if(product.stock < quantity) throw Error ('No hay stock disponible')
+        if (product.stock < quantity) throw Error('No hay stock disponible')
 
-        const userCart = await Cart.findOne({user:userId})
+        const userCart = await Cart.findOne({ user: userId })
         if (!userCart) throw Error('No existe el carrito')
-        
-        userCart.products.push({product:id, quantity, price:product.price})
+
+        userCart.products.push({ product: id, quantity, price: product.price })
         await userCart.save()
 
         return res.status(200).send('Producto agregado correctamente!')
@@ -22,4 +34,4 @@ const addProduct = async (req,res) => {
     }
 }
 
-export default addProduct
+export { addProduct, getCart }
