b2e4fccf0f958198a48a514e7b418cf6b133e2e5
Mauricio Barca FT37a <mauriciobarca1989@gmail.com>
2023-06-18 12:09:13 +0200

    Update app.js
    moved cors configuration to the top

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ import { fileURLToPath } from 'url';
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(cors())
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize())
-app.use(cors())
 
 app.use(
   fileUpload({
